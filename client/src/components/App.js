4cf8fb7469b7756076c11e311b92d08277dbf5f8
Cody Nicoll <cdnicoll@gmail.com>
2019-10-20 15:05:14 -0700

    Added header component and styled it

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,13 +5,15 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='ui container'>
         <BrowserRouter>
           <div>
+            <Header />
             <Route path='/' exact component={StreamList} />
             <Route path='/streams/new' exact component={StreamCreate} />
             <Route path='/streams/edit' exact component={StreamEdit} />
