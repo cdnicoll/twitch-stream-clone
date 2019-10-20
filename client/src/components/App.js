b4f46c95c6429cd0fa93b82634dfce34ba67a2e6
Cody Nicoll <cdnicoll@gmail.com>
2019-10-20 14:58:20 -0700

    Added semantic-ui style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,17 +9,17 @@ import StreamShow from './streams/StreamShow';
 class App extends React.Component {
   render() {
     return (
-    <div>
+      <div>
         <BrowserRouter>
-            <div>
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit" exact component={StreamCreate} />
-                <Route path="/streams/delete" exact component={StreamCreate} />
-                <Route path="/streams/show" exact component={StreamCreate} />
-            </div>
+          <div>
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' exact component={StreamCreate} />
+            <Route path='/streams/edit' exact component={StreamEdit} />
+            <Route path='/streams/delete' exact component={StreamDelete} />
+            <Route path='/streams/show' exact component={StreamShow} />
+          </div>
         </BrowserRouter>
-    </div>
+      </div>
     );
   }
 }
