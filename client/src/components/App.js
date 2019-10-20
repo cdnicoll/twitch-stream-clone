007eb94a9f2bd279bf6084c56e8cd89390b1df04
Cody Nicoll <cdnicoll@gmail.com>
2019-10-20 14:22:24 -0700

    Added router via yarn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 class App extends React.Component {
-
-	render() {
-		return (
-			<div>Hello World</div>
-		)
-	}
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
 export default App;
