30a3ec7e7b4bb2dd1ef701a4965e7237a1ef76ba
Cody Nicoll <cdnicoll@gmail.com>
2019-10-27 09:58:44 -0700

    bug fix

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,32 +8,28 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  renderList() {
+  renderList = () => {
     const { streams } = this.props;
-    streams.map(stream => {
+    return streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
           <i className='large middle aligned icon camera' />
           <div className='content'>
-              { stream.title }
-              <div className="description">
-                  { stream.description }
-              </div>
+            {stream.title}
+            <div className='description'>{stream.description}</div>
           </div>
         </div>
       );
     });
-  }
+  };
 
   render() {
     return (
-        <div>
-            <h2>Streams</h2>
-            <div className="ui celled list">
-                { this.renderList() }
-            </div>
-        </div>
-    )
+      <div>
+        <h2>Streams</h2>
+        <div className='ui celled list'>{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
