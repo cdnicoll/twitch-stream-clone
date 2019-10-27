350999b805eca93f8608cdbba911e64f7be8a1a5
Cody Nicoll <cdnicoll@gmail.com>
2019-10-27 10:28:15 -0700

    Changed how the app handles the history route object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
+import history from '../history'
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -12,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='ui container'>
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />
             <Route path='/' exact component={StreamList} />
@@ -21,7 +22,7 @@ class App extends React.Component {
             <Route path='/streams/delete' exact component={StreamDelete} />
             <Route path='/streams/show' exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
